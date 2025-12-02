1d90be3d0e38908f84ad2d3e97dd261c5b3ca8e2
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2025-12-02 13:39:27 +0700

    refactor: folder structure

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,9 +1,9 @@
 import { Component, useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+// import "./App.css";
 
-import Heading from "./components/Heading";
+import Heading from "../components/Heading";
 
 function App() {
   const [count, setCount] = useState(0);
