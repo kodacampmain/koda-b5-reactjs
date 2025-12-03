b32bfcfa72d3c488963a79de1219de6d260877f9
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2025-12-03 16:41:04 +0700

    refactor: multi state form to single state form

--- src/components/AddTodoList.jsx
+++ src/components/AddTodoList.jsx
@@ -13,26 +13,40 @@ import { useState } from "react";
  */
 function AddTodoList(props) {
   const { changeTodos } = props;
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [content, setContent] = useState("");
+  const [form, setForm] = useState({
+    title: "",
+    content: "",
+    // isCompleted: false,
+  });
   const submitHandler = (event) => {
     event.preventDefault();
     const newTodo = {};
     // validasi tidak boleh kosong title
     // jika error maka stop
     // Object.assign(newTodo, { title: event.target["todo-title"].value });
-    Object.assign(newTodo, { title });
+    Object.assign(newTodo, { title: form.title });
     // validasi tidak boleh kosong content
     // jika error maka stop
     // Object.assign(newTodo, { content: event.target["todo-content"].value });
-    Object.assign(newTodo, { content });
+    Object.assign(newTodo, { content: form.content });
     changeTodos((todos) => {
       return [...todos, newTodo];
     });
     // event.target["todo-title"].value = "";
-    setTitle("");
+    // setTitle("");
     // event.target["todo-content"].value = "";
-    setContent("");
+    // setContent("");
+    setForm({
+      title: "",
+      content: "",
+    });
+  };
+  const onChangeHandler = (e) => {
+    setForm((form) => {
+      return { ...form, [e.target.id]: e.target.value };
+    });
   };
   return (
     <aside className="w-[20vw] p-5 border-2 border-solid border-black">
@@ -42,10 +56,10 @@ function AddTodoList(props) {
           <label htmlFor="title">Title</label>
           <input
             type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
             name="todo-title"
             id="title"
+            value={form.title}
+            onChange={onChangeHandler}
             className="border-2 border-solid border-black p-1 w-full"
           />
         </div>
@@ -54,8 +68,8 @@ function AddTodoList(props) {
           <textarea
             name="todo-content"
             id="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            value={form.content}
+            onChange={onChangeHandler}
             className="border-2 border-solid border-black p-1 w-full h-[200px] resize-none"
           ></textarea>
         </div>
