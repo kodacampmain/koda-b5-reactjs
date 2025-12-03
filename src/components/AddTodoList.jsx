7b85140f4acc33251ef558343d04fbd90a691448
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2025-12-03 15:09:25 +0700

    refactor: change uncontrolled input to controlled input on add todo form

--- src/components/AddTodoList.jsx
+++ src/components/AddTodoList.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 /**
  * Todo Object
  * @typedef {Object} todo
@@ -12,20 +13,26 @@
  */
 function AddTodoList(props) {
   const { changeTodos } = props;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const submitHandler = (event) => {
     event.preventDefault();
     const newTodo = {};
     // validasi tidak boleh kosong title
     // jika error maka stop
-    Object.assign(newTodo, { title: event.target["todo-title"].value });
+    // Object.assign(newTodo, { title: event.target["todo-title"].value });
+    Object.assign(newTodo, { title });
     // validasi tidak boleh kosong content
     // jika error maka stop
-    Object.assign(newTodo, { content: event.target["todo-content"].value });
+    // Object.assign(newTodo, { content: event.target["todo-content"].value });
+    Object.assign(newTodo, { content });
     changeTodos((todos) => {
       return [...todos, newTodo];
     });
-    event.target["todo-title"].value = "";
-    event.target["todo-content"].value = "";
+    // event.target["todo-title"].value = "";
+    setTitle("");
+    // event.target["todo-content"].value = "";
+    setContent("");
   };
   return (
     <aside className="w-[20vw] p-5 border-2 border-solid border-black">
@@ -33,13 +40,22 @@ function AddTodoList(props) {
         <header className="text-center font-bold text-xl">Add Todo</header>
         <div>
           <label htmlFor="title">Title</label>
-          <input type="text" name="todo-title" id="title" className="border-2 border-solid border-black p-1 w-full" />
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            name="todo-title"
+            id="title"
+            className="border-2 border-solid border-black p-1 w-full"
+          />
         </div>
         <div>
           <label htmlFor="content">Content</label>
           <textarea
             name="todo-content"
             id="content"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
             className="border-2 border-solid border-black p-1 w-full h-[200px] resize-none"
           ></textarea>
         </div>
