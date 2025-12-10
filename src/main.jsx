d50949dcf75f661872c7d62384dfa263a20d24b6
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2025-12-10 18:10:52 +0700

    feat: add persistence for todos reducer using redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { Provider as ReduxProvider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 // import "./index.css";
 // import App from "./pages/App";
@@ -10,18 +11,26 @@ import { Provider as ReduxProvider } from "react-redux";
 import Router from "./Router";
 import ThemeProvider from "./contexts/theme/ThemeProvider";
 import TodoProvider from "./contexts/todos/TodoProvider";
-import reduxStore from "./redux/store";
+import reduxStore, { persistedStore } from "./redux/store";
+import Loader from "./components/Loader";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ReduxProvider store={reduxStore}>
-      <ThemeProvider>
-        <TodoProvider>
-          <BrowserRouter>
-            <Router />
-          </BrowserRouter>
-        </TodoProvider>
-      </ThemeProvider>
+      <PersistGate
+        persistor={persistedStore}
+        loading={
+          <Loader className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+        }
+      >
+        <ThemeProvider>
+          <TodoProvider>
+            <BrowserRouter>
+              <Router />
+            </BrowserRouter>
+          </TodoProvider>
+        </ThemeProvider>
+      </PersistGate>
     </ReduxProvider>
   </StrictMode>,
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,39 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistCombineReducers,
+  persistStore,
+  PERSIST,
+  REHYDRATE,
+  FLUSH,
+  REGISTER,
+  PURGE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import todosReducer from "./slices/todos.slice";
 import pokemonReducer from "./slices/pokemon.slice";
 
-const store = configureStore({
-  reducer: {
-    todos: todosReducer,
-    pokemon: pokemonReducer,
-  },
-  devTools: import.meta.env.VITE_MODE === "development" ? true : false,
+const persistConfig = {
+  key: "koda5-redux",
+  storage,
+  whitelist: ["todos"],
+};
+
+const persistedReducer = persistCombineReducers(persistConfig, {
+  todos: todosReducer,
+  pokemon: pokemonReducer,
 });
 
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: import.meta.env.VITE_MODE === "development" ? true : false,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [PERSIST, REHYDRATE, FLUSH, REGISTER, PURGE],
+      },
+    }),
+});
+
+export const persistedStore = persistStore(store);
 export default store;
